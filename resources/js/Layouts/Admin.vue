8c727ce2cd46b94f0a87dcd922dc9c3bf45170fe
Munish Rana <munishmr2007@gmail.com>
2021-09-07 02:43:00 +0530

    Third Commit

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -3,9 +3,9 @@
         <div class="wrapper">
 
             <!-- Preloader -->
-            <div class="preloader flex-column justify-content-center align-items-center">
+            <!-- <div class="preloader flex-column justify-content-center align-items-center">
                 <img class="animation__wobble" :src="'/storage/images/AdminLTELogo.png'" alt="AdminLTELogo" height="60" width="60">
-            </div>
+            </div> -->
 
             <!-- Navbar -->
             <NavBar />
@@ -52,7 +52,7 @@
             SideBar,
         },
         mounted() {
-            this.init()
+            // this.init()
         },
         methods: {
             init() {
